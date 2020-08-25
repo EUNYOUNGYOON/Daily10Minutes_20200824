ab289a21aa0006fc12020fe6aa7f29126fd55c22
asdf <milkoon1@naver.com>
2020-08-25 20:51:22 +0900

    로그인성공 여부에 따라 실행

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
@@ -1,13 +1,24 @@
 package kr.co.tjoeun.daily10minutes_20200824
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kr.co.tjoeun.daily10minutes_20200824.utils.ServerUtil
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+    }
+
     override fun setupEvents() {
 
         loginBtn.setOnClickListener {
@@ -18,27 +29,38 @@ class MainActivity : BaseActivity() {
             // new 인터페이스()
             ServerUtil.postRequestLogin(inputId,inputPw, object :ServerUtil.JsonResponseHandler {
                 override fun onRespnse(json: JSONObject) {
+
                     //실제로 응답이 돌아왔을 때 실행시켜줄 내용들
+                    // 아이디 비번 : kj_cho@nepp.kr / Test!1234
                     Log.d("메인화면에서 응답확인", json.toString())
+
+                    val codeNum = json.getInt("code")
+                    if(codeNum == 200){
+                        // 서버 개발자가 로그인 성공일 때는 code를 200으로 준다.
+                        // 로그인 성공시에 대한 코드 정의
+                        Log.d("로그인시도", "성공상황")
+
+                        // 그냥 쓰면 앱 죽고 runOnUiThread 안에서 실행시켜야한다.
+                        runOnUiThread {
+                            Toast.makeText(mContext,"로그인 성공", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+                    else {
+                        // 그 외의 숫자는 실패
+                        Log.e("로그인시도", "실패상황")
+                        runOnUiThread {
+                            Toast.makeText(mContext,"로그인 실패", Toast.LENGTH_SHORT).show()
+                        }
+                    }
                 }
-
             })
-
-
-
         }
 
+    }
+
+    override fun setValues() {
 
     }
 
-override fun setValues() {
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        setupEvents()
-        setValues()
-    }
 }
