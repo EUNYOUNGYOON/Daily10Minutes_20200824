d147c901714a6afdd004f123372e148f37a78c35
asdf <milkoon1@naver.com>
2020-08-27 21:14:11 +0900

    실제로 목록에 ㄷ마긴 프로젝트 목록을 리스트뷰에 출력 아직 실데이터 반영은 안된 상태

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/MainActivity.kt
@@ -3,17 +3,23 @@ package kr.co.tjoeun.daily10minutes_20200824
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.daily10minutes_20200824.adapters.ProjectAdapter
 import kr.co.tjoeun.daily10minutes_20200824.datas.Project
 import kr.co.tjoeun.daily10minutes_20200824.utils.ServerUtil
+import okhttp3.internal.notify
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
+    lateinit var mProjectAdapter: ProjectAdapter
     val mProjectList = ArrayList<Project>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
@@ -22,6 +28,8 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
         getProjectListFromServer()
+        mProjectAdapter = ProjectAdapter(mContext, R.layout.project_list_item, mProjectList)
+        projectListView.adapter = mProjectAdapter
     }
 
     fun getProjectListFromServer(){
@@ -43,16 +51,17 @@ class MainActivity : BaseActivity() {
                     project.desc = projectObj.getString("description")
 
                     mProjectList.add(project)
-
-
-
                 }
 
+                // 비동기처리 -> 어댑터 먼저 끝나고, 프로젝트 목록이 나중에 추가될 수 있다.
+                // 리스트뷰의 입장에서는 내용물 목록이 변경된 상황 -> notifyDataSetChanged()로 새로고침
+                // 그대로 쓰면 앱이 죽어버림
+                // 새로고침 : ui에 영향을 주겠다 -> runOnUiThread { } 안에다가 무조건 써야한다.
+                runOnUiThread {
+                    mProjectAdapter.notifyDataSetChanged()
+                }
 
             }
-
         })
     }
-
-
 }
