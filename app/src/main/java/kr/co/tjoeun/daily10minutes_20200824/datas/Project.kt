12d9a66700a898c25b554def1bf1584689a3be84
EUNYOUNGYOON <milkoon1@naver.com>
2020-09-14 22:21:27 +0900

    last_status 관련해서 프로젝트 최근 시도 상태 파싱하는 것 - 서버의 JSOn null 체크 후 파싱

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/datas/Project.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/datas/Project.kt
@@ -12,6 +12,8 @@ class Project : Serializable {
     var ongoing_users_count = 0
     var proof_method = ""
 
+    var myLastStatus : String? = null
+
     companion object {
 
         fun getProjectFromJson(json: JSONObject) : Project {
@@ -25,6 +27,14 @@ class Project : Serializable {
             proj.desc = json.getString("description")
             proj.ongoing_users_count = json.getInt("ongoing_users_count")
             proj.proof_method = json.getString("proof_method")
+
+            // 서버에서 주는 데이터가 null 일 수도 있거나
+            // 아예 상황에 따라 첨부되지 않을 수도 있는 항목은
+            // isNull로 체크해서 null 아닐 때만 파싱하는것이 앱의 안정성을 높인다
+
+            if(!json.isNull("my_last_status")) {
+                proj.myLastStatus = json.getString("my_last_status")
+            }
 
             return proj
         }
