40c8b0f2e46aa1ab071b73cc2bc3e71befd745bf
EUNYOUNGYOON <milkoon1@naver.com>
2020-09-15 20:15:12 +0900

    프로젝트 참여자 목록 확인 화면추가

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ProjectDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ProjectDetailActivity.kt
@@ -1,6 +1,7 @@
 package kr.co.tjoeun.daily10minutes_20200824
 
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -23,6 +24,12 @@ class ProjectDetailActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        viewAllMemsBtn.setOnClickListener {
+
+            val myIntent = Intent(mContext, ViewProjectMembersActivity::class.java)
+            startActivity(myIntent)
+        }
 
         // 신청하기 버튼을 누르면 진짜 신청할건지? 확인(AlertDialog)되면 신청처리
         applyBtn.setOnClickListener {
