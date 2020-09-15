49fc9d779d8216984a231fdaa67ee5fa7a4dc456
EUNYOUNGYOON <milkoon1@naver.com>
2020-09-15 21:37:09 +0900

    사용자 파싱할 때 프사 경로도 파싱해서 목록에 추가하는 기능

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/datas/User.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/datas/User.kt
@@ -23,6 +23,19 @@ class User {
             user.email = json.getString("email")
             
             // 사용자 목록에도 파싱을 해줘야함
+            // json 변수 내의 profile_images JSONArray를 추가로 파싱하자
+
+            val pfImgJsonArr = json.getJSONArray("profile_images")
+            for (i in 0 until pfImgJsonArr.length()) {
+
+                // 프사 정보를 담고 있는 JSONObject 하나하나 추출
+                val pfImgObj = pfImgJsonArr.getJSONObject(i)
+
+                // 따낸 JSONObject 안에서 이미지 주소 String 만 추출 -> user의 프사 목록에 추가
+                val imageUrl = pfImgObj.getString("img_url")
+                user.profileImageArrayList.add(imageUrl)
+
+            }
 
 
             return user
